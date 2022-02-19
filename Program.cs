755242c9bd13dc4b5ef19ad280c85124edfba426
Илья <iliya.guschin@bk.ru>
2022-02-19 12:07:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab_20_Delegates
+{
+    class Program
+    {
+        delegate void MyDelegate(double R);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = new MyDelegate(ArcLength);
+            myDelegate += CircleArea;
+            myDelegate += SphereValue;
+
+            myDelegate?.Invoke(5);
+
+        }
+
+        static void ArcLength (double R)
+        {
+            Console.WriteLine("\nДлина дуги {0}", 2 * System.Math.PI * R);
+        }
+
+        static void CircleArea(double R)
+        {
+            Console.WriteLine("\nПлощадь круга {0}", System.Math.PI * System.Math.Pow(R,2));
+        }
+
+        static void SphereValue(double R)
+        {
+            Console.WriteLine("\nОбъем шара {0}", (4/3)*System.Math.PI * System.Math.Pow(R, 3));
+        }
+
+    }
+}
